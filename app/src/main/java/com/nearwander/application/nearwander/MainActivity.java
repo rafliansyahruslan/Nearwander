f5b502cfede80d37aaba7273f091a2d4fc775d87
rafliansyahruslan <rafliansyahruslan@gmail.com>
2017-03-08 08:36:05 +0700

    Login-layout-LogoAdded

--- app/src/main/java/com/nearwander/application/nearwander/MainActivity.java
+++ app/src/main/java/com/nearwander/application/nearwander/MainActivity.java
@@ -2,11 +2,13 @@ package com.nearwander.application.nearwander;
 //Byan Gilar Naufal Rafli Zulkarnain
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -34,11 +36,17 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "MAIN_ACTIVITY";
 
+    TextView logo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (TextView) findViewById(R.id.logo);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"fonts/JosefinSans-Bold.ttf");
+        logo.setTypeface(myCustomFont);
 
 
         mAuth = FirebaseAuth.getInstance();
